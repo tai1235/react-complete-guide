13e994838ca2ae04f61574ab1fd60f79d6dcf656
tainv <tainv@lumi.biz>
2019-09-07 20:23:11 +0700

    Setting Styles Dynamically

--- src/App.js
+++ src/App.js
@@ -45,7 +45,8 @@ class App extends Component {
 
     render() {
         const style = {
-            backgroundColor: 'white',
+            backgroundColor: 'green',
+            color: 'white',
             font: 'inherit',
             border: '1px solid blue',
             padding: '8px'
@@ -65,6 +66,8 @@ class App extends Component {
                     />)}
                 </div>
             );
+
+            style.backgroundColor = 'red';
         }
 
         return (
