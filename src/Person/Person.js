1ced5d58b2f3538b8660ad5d9dd0845c350a07f6
tainv <tainv@lumi.biz>
2019-09-07 21:14:13 +0700

    Enabling & Using CSS Modules (after ejection and reconfiguration)

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import './Person.css';
+import classes from './Person.css';
 
 const person = (props) => {
     return (
-        <div className={"Person"} >
+        <div className={classes.Person}>
             <p onClick={props.click}>My name is {props.name} and I'm {props.age} years old</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>
