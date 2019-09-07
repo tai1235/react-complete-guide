2117eeab6293646810b66c907728e38ec26cc4f0
tainv <tainv@lumi.biz>
2019-09-07 20:59:26 +0700

    Enabling & Using CSS Modules (before npm run eject)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Radium, {StyleRoot} from 'radium'
 import './App.css';
 import Person from './Person/Person';
 
@@ -51,11 +50,7 @@ class App extends Component {
             font: 'inherit',
             border: '1px solid blue',
             padding: '8px',
-            cursor: 'pointer',
-            ':hover': {
-                backgroundColor: 'lightgreen',
-                color: 'black'
-            }
+            cursor: 'pointer'
         };
 
         let persons = null;
@@ -74,10 +69,6 @@ class App extends Component {
             );
 
             style.backgroundColor = 'red';
-            style[':hover'] = {
-                backgroundColor: 'salmon',
-                color: 'black'
-            }
         }
 
         const classes = [];
@@ -89,22 +80,20 @@ class App extends Component {
         }
 
         return (
-            <StyleRoot>
-                <div className="App">
-                    <h1>Hi, I'm a React App</h1>
-                    <p className={classes.join(' ')}>This is really working</p>
-                    <button
-                        style={style}
-                        onClick={this.togglePersonHandler}>
-                        Toggle Persons
-                    </button>
-                    {persons}
-                </div>
-            </StyleRoot>
+            <div className="App">
+                <h1>Hi, I'm a React App</h1>
+                <p className={classes.join(' ')}>This is really working</p>
+                <button
+                    style={style}
+                    onClick={this.togglePersonHandler}>
+                    Toggle Persons
+                </button>
+                {persons}
+            </div>
         );
         // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App'))
     }
 
 }
 
-export default Radium(App);
+export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,16 +1,9 @@
 import React from 'react';
-import Radium from 'radium';
 import './Person.css';
 
 const person = (props) => {
-    const style = {
-        '@media (min-width: 500px)': {
-            width: '450px'
-        }
-    };
-
     return (
-        <div className={"Person"} style={style}>
+        <div className={"Person"} >
             <p onClick={props.click}>My name is {props.name} and I'm {props.age} years old</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>
@@ -18,4 +11,4 @@ const person = (props) => {
     );
 };
 
-export default Radium(person);
+export default person;
