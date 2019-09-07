e5a6ed1ed63ab1cb0415c3058f1a1434c0ed8dd1
tainv <tainv@lumi.biz>
2019-09-07 21:30:51 +0700

    Adding Pseudo Selectors and Working with Media Queries

--- src/App.js
+++ src/App.js
@@ -44,16 +44,8 @@ class App extends Component {
     };
 
     render() {
-        const style = {
-            backgroundColor: 'green',
-            color: 'white',
-            font: 'inherit',
-            border: '1px solid blue',
-            padding: '8px',
-            cursor: 'pointer'
-        };
-
         let persons = null;
+        let buttonClass = '';
 
         if (this.state.showPerson) {
             persons = (
@@ -68,7 +60,7 @@ class App extends Component {
                 </div>
             );
 
-            style.backgroundColor = 'red';
+            buttonClass = classes.Red;
         }
 
         const assignedClasses = [];
@@ -84,7 +76,7 @@ class App extends Component {
                 <h1>Hi, I'm a React App</h1>
                 <p className={assignedClasses.join(' ')}>This is really working</p>
                 <button
-                    style={style}
+                    className={buttonClass}
                     onClick={this.togglePersonHandler}>
                     Toggle Persons
                 </button>
